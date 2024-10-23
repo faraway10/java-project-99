a6cf59ca84f4fd8b5b45d19d95cef71363e30f87
faraway10 <42tree42@gmail.com>
2024-10-23 05:05:13 +0300

    User model added; configuration profiles for development and production created

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("io.freefair.lombok") version "8.6"
 }
 
 group = "hexlet.code"
@@ -30,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-configuration-processor")
 	//	implementation("org.springframework.boot:spring-boot-starter-security")
 	//	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.postgresql:postgresql:42.7.4")
 
 	runtimeOnly("com.h2database:h2:2.2.224")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
